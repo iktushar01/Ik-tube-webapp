8c0ace0eecb8391abd6312fede84604ec3d5f028
tushar <iktushar2020@gmail.com>
2025-03-12 07:35:13 +0600

    added loadCategories function

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log("index is connected");
+function loadCategories (){
+    //fetch the data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+}
 
+function displayCategories (categories){
+    console.log(categories);
+}
+
+loadCategories()
