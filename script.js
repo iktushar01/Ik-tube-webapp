62451cce589220e7d045ab0ea1391628c6133762
tushar <iktushar2020@gmail.com>
2025-03-13 10:38:54 +0600

    added moral in details button

--- script.js
+++ script.js
@@ -40,6 +40,33 @@ const loadCategoryVideo = (id) => {
   });
 }
 
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayVideoDetails(data.video));
+}
+
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details_container");
+  detailsContainer.innerHTML = `
+  <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>${video.description}</p>
+  </div>
+</div>
+  `
+}
+
 function displayCategories(categories) {
   // get the container
   const categoryContainer = document.getElementById("category_container");
@@ -96,7 +123,7 @@ const displayVideos = (videos) => {
               </div>
               
               </div>
-             
+             <button onclick = loadVideoDetails("${video.video_id}") class="btn btn-block">Details</button>
             </div>
           
         `;
